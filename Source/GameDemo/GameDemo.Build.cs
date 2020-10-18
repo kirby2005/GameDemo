6ef3531e48172fee71709e57d870f9b8ccf0c492
kirbybaobao <kirbybaobao@gmail.com>
2020-10-18 18:51:15 +0800

    import slua_unreal

--- Source/GameDemo/GameDemo.Build.cs
+++ Source/GameDemo/GameDemo.Build.cs
@@ -10,11 +10,14 @@ public class GameDemo : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "slua_unreal", "Slate", "SlateCore", "UMG" });
+
+		PrivateIncludePathModuleNames.AddRange(new string[] { "slua_unreal" });
+		PublicIncludePathModuleNames.AddRange(new string[] { "slua_unreal" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
